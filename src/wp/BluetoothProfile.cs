0b1ea865c6eee98894ae5397a2dcd252560b8c0a
rroes <rroesinger@inconso.de>
2017-01-20 16:45:33 +0100

    xml

---
+++ src/wp/BluetoothProfile.cs
@@ -0,0 +1,22 @@
+﻿/* Copyright by Apitron LTD 2014,
+ The code is free to use in any project.
+ Author Robert McKalkin.*/
+
+namespace Apitron.Bluetooth
+{
+    /// <summary>
+    /// Defines bluetooth profiles that  <see cref="BluetoothManager"/> is able to use.
+    /// </summary>
+    enum BluetoothProfile
+    {
+        /// <summary>
+        /// The OBEX Object Push Profile.
+        /// </summary>
+        OBEXOPP,
+
+        /// <summary>
+        /// The OBEX File Transfer Profile, please not that support ffor this
+        /// </summary>
+        OBEXFTP
+    }
+}
